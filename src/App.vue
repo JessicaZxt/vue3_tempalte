8fe0c2b74e00b88c65a25c8f3779f85cff6fad3d
leo <leo@hyditech.com>
2023-12-19 13:42:05 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>我是App</h1>
+</template>
+
+<script setup lang="ts">
+// import { reactive, ref } from 'vue';
+const str = '2432134'
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 100px;
+  h1 {
+    color: #000;
+  }
+}
+</style>
